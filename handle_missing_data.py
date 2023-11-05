698cc3556deb05efeb35ece12564b5380386bd4e
6-6ho <6_6ho@kakao.com>
2023-11-06 08:03:52 +0900

    Update README.md

--- handle_missing_data.py
+++ handle_missing_data.py
@@ -1,30 +1,31 @@
 import pandas as pd
 
-df = pd.read_csv("Recent_10_Year_Outbound.csv", na_values=[''])
+# CSV 파일 경로
+csv_path = "Recent_10_Year_Outbound.csv"
 
+# CSV 파일 불러오기
+df = pd.read_csv(csv_path, na_values=['', ' '])
+
+# 연도별 출국자수 증가율 리스트
 yearly_changes = [8.3, 20.1, 15.9, 18.4, 8.3, 0.1, -85.1, -71.4, 436.1, 420.2]
 
-missing_values_by_country = df.isna().sum()
+# 결측치 채우기
+for col in df.columns[1:]:
+    for i in range(1, len(df)):
+        if pd.isnull(df.at[i, col]):
+            # 이전 년도로 결측치를 채우기 시도
+            df.at[i, col] = df.at[i-1, col] * (1 + yearly_changes[i-1] / 100)
+            
+            # 만약 이전 년도로 결측치를 채웠는데 그 결과가 결측치라면 다음 년도로 시도
+            if pd.isnull(df.at[i, col]):
+                df.at[i, col] = df.at[i+1, col] * (1 + yearly_changes[i] / 100)
 
+# 결과 출력
 print("각 국가별 결측치 개수:")
-print(missing_values_by_country)
+print(df.isna().sum())
 
-def fill_missing_values(row, idx, df, changes):
-    for country in df.columns[1:]:  # 'Year' 열 이후의 모든 국가에 대해 반복
-        if pd.isna(row[country]):
-            # 이전 연도 값으로부터 채우기
-            if idx > 0 and pd.notna(df.at[idx - 1, country]):
-                prev_year_value = df.at[idx - 1, country]
-                row[country] = prev_year_value * (1 + changes[idx - 1] / 100)
-            # 다음 연도 값으로부터 채우기
-            elif idx < len(df) - 1 and pd.notna(df.at[idx + 1, country]):
-                next_year_value = df.at[idx + 1, country]
-                row[country] = next_year_value / (1 + changes[idx] / 100)
-    return row
-
-df = df.apply(lambda row: fill_missing_values(row, row.name, df, yearly_changes), axis=1)
-
-output_file_path = "결과를_저장할_파일_경로.csv"
+# 결과 저장
+output_file_path = "Recent_10_Year_Outbound_Not_Nan.csv"
 df.to_csv(output_file_path, index=False)
 
 print("결과가 저장된 파일 경로:", output_file_path)
