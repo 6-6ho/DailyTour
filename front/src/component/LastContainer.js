25e7b538216951a99d9a1f3439add715279cfc42
eunnzy <silverg12@skuniv.ac.kr>
2023-11-13 10:37:56 +0900

    Changes to be committed: 	new file:   front/public/img/airplane_icon.png 	modified:   front/src/component/Bar.css 	modified:   front/src/component/Dashboard.css 	modified:   front/src/component/LastContainer.js 	modified:   front/src/component/Sidebar.js 	new file:   front/src/component/Tourboard.js

--- front/src/component/LastContainer.js
+++ front/src/component/LastContainer.js
@@ -1,4 +1,19 @@
+import './Dashboard.css'
+import './Grid.css'
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+
 
 export default function LastContainer() {
-    
+    return(
+        <div className="App">
+            <div className="col-2">
+                <Sidebar></Sidebar>
+            </div>
+            <div className="col-8 right-wrap">
+                <Navbar></Navbar>
+                
+            </div>
+        </div>
+    )    
 }
--- front/src/component/Sidebar.js
+++ front/src/component/Sidebar.js
@@ -2,20 +2,18 @@ import './Bar.css'
 
 export default function Sidebar() {
     return(
-    <div className='side-bar-wrap'>
-        <div className='side-bar'>
-            <div className='logo-wrap'>
-                <div className='logo-box-wrap'>
-
-                    
+        <div className='side-bar-wrap'>
+            <div className='side-bar'>
+                <div className='logo-wrap'>
+                    <div className='logo-box-wrap'>
+                        <img src='img/airplane_icon.png'></img>
+                    </div>
+                    <span className='logo-font'>
+                        Daily Tour
+                    </span>
                 </div>
-                <span className='logo-font'>
-                    Daily Trip
-                </span>
             </div>
-
         </div>
-    </div>
     
     )
 }
