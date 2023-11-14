9eb8873d84f07220f3104c99d309cfb982649f28
eunnzy <silverg12@skuniv.ac.kr>
2023-11-14 14:07:52 +0900

    Changes to be committed: 	new file:   back/src/main/java/com/daily/tour/Config.java 	new file:   back/src/main/java/com/daily/tour/Main.java 	new file:   back/src/main/java/com/daily/tour/controller/AccomController.java 	new file:   back/src/main/resources/application.yaml
    Changes not staged for commit:
    	deleted:    AgodaCrawler.py
    	modified:   back/src/main/java/com/daily/tour/Config.java
    	modified:   back/src/main/java/com/daily/tour/controller/AccomController.java
    	modified:   back/src/main/resources/application.yaml
    
    Untracked files:
    	OutBound_Data/
    	TravelTimesCrawler.py
    	agoda_crawler.py
    	back/.gitignore
    	back/.idea/
    	back/build.gradle
    	back/gradle/
    	back/gradlew
    	back/gradlew.bat
    	back/settings.gradle
    	handle_missing_month_data.py
    	news_wordcloud.py
    	travel_news.json

---
+++ back/src/main/java/com/daily/tour/Config.java
@@ -0,0 +1,9 @@
+package com.daily.tour;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+}
