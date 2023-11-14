9feeda0b1a7ad065638ca6d2061484f30fb13412
eunnzy <silverg12@skuniv.ac.kr>
2023-11-14 15:24:21 +0900

    Changes to be committed: 	deleted:    back/.gitignore 	deleted:    back/.idea/.gitignore 	deleted:    back/.idea/gradle.xml 	deleted:    back/.idea/misc.xml 	deleted:    back/.idea/uiDesigner.xml 	deleted:    back/.idea/vcs.xml 	deleted:    back/build.gradle 	deleted:    back/gradle/wrapper/gradle-wrapper.jar 	deleted:    back/gradle/wrapper/gradle-wrapper.properties 	deleted:    back/gradlew 	deleted:    back/gradlew.bat 	deleted:    back/settings.gradle 	deleted:    back/src/main/java/com/daily/tour/Config.java 	deleted:    back/src/main/java/com/daily/tour/Main.java 	deleted:    back/src/main/java/com/daily/tour/controller/AccomController.java 	deleted:    back/src/main/resources/application.yaml

--- back/src/main/java/com/daily/tour/Config.java
+++
@@ -1,9 +0,0 @@
-package com.daily.tour;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class Config implements WebMvcConfigurer {   // 설정 클래스
-
-}
--- back/src/main/java/com/daily/tour/controller/AccomController.java
+++
@@ -1,7 +0,0 @@
-package com.daily.tour.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AccomController {
-}
