a4b878354305198b24c164c545d48eb4b15066e0
eunnzy <silverg12@skuniv.ac.kr>
2023-11-28 14:11:59 +0900

    Changes to be committed: 	modified:   back/.idea/gradle.xml 	modified:   back/.idea/misc.xml 	modified:   back/src/main/java/com/daily/tour/controller/CountryStatController.java 	modified:   back/src/main/java/com/daily/tour/controller/ExchangeRateAPIController.java 	modified:   back/src/main/java/com/daily/tour/mapper/AccomodationMapper.java 	modified:   back/src/main/java/com/daily/tour/mapper/CountryStatMapper.java 	modified:   back/src/main/java/com/daily/tour/service/AccomodationService.java 	modified:   back/src/main/resources/application.yaml 	modified:   front/src/App.js 	modified:   front/src/component/Bar.css 	modified:   front/src/component/Dashboard.css 	modified:   front/src/component/Dashboard.js 	modified:   front/src/component/SideChart.js 	modified:   front/src/component/Tourboard.js

--- front/src/component/SideChart.js
+++ front/src/component/SideChart.js
@@ -1,14 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import {Line, Bar} from 'react-chartjs-2';
+import {Link} from "react-router-dom"
+import { localDomain } from './common';
+import {
+    Chart,
+    LineController,
+    BarController,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement
+  } from "chart.js";
+Chart.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
 
 export default function SideChart() {
+    const [monthList, setMonthList] = useState([]); // 월별 rank
+    const [yearList, setYearList] = useState([]);   // 연도별 rank
+    
+    useEffect( ()=>  {  
+        fetch(`${localDomain}/country/month/8`)  // JSON-Server 에게 students data 요청
+	        .then( res => { return res.json() } ) 
+	        .then( data => {
+               console.log(data);
+               setMonthList(data);
+             } 
+        ); 
+    }, []);
+
+    useEffect( ()=>  {  
+        fetch(`${localDomain}/country/year/2023`)  // JSON-Server 에게 students data 요청
+	        .then( res => { return res.json() } ) 
+	        .then( data => {
+               console.log(data);
+               setYearList(data);
+             } 
+        ); 
+    }, []);
+
+
     return (
         <div className="side-chart-box-container">
             <div className="side-chart-box">
-
+                <ol className="side-ol-style">
+                    {
+                        monthList.map( (month) => (
+                            <li className key={month.cntCode}> <Link to={"/country/" + month.cntCode }> {month.cntName} </Link> </li>
+                        ))
+                    }
+                    
+                </ol>
             </div>
             <div className="side-chart-box">
-                <div>
+            <ol className="side-ol-style">
+                    {
+                        yearList.map( (year) => (
+                            <li className key={year.cntCode}> <Link to={"/country/" + year.cntCode }> {year.cntName} </Link> </li>
+                        ))
+                    }
                     
-                </div>
+                </ol>
             </div>
         </div>
     )
