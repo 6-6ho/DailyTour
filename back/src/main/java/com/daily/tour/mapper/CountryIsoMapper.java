54645e644536f9d1d94118d628019751c5d237a5
eunnzy <silverg12@skuniv.ac.kr>
2023-12-18 11:39:32 +0900

    On branch eunji Your branch is up to date with 'origin/eunji'.
    Changes to be committed:
    	modified:   back/src/main/java/com/daily/tour/mapper/CountryIsoMapper.java

--- back/src/main/java/com/daily/tour/mapper/CountryIsoMapper.java
+++ back/src/main/java/com/daily/tour/mapper/CountryIsoMapper.java
@@ -13,7 +13,7 @@ public interface CountryIsoMapper {
     @Select({"<script> SELECT distinct CNT_CODE as cntCode, ISO_CODE as isoCode " +
             "FROM country_iso_tb " +
             "WHERE CNT_CODE IN <foreach item='item' collection='cntCodeList' open='(' separator=',' close=')'>" +
-            "#{item}" +
+            "#{item} </foreach>"  +
             "</script>"})
     List<CountryIso> findCountryIsoCode(List<String> cntCodeList);  // 국제 표준으로 변환
 }
